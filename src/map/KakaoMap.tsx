f14ef19f474887b62804e52dd187b0264ede3479
gulggum <163222604+gulggum@users.noreply.github.com>
2025-12-05 10:19:29 +0900

    env파일따옴표수정

--- src/map/KakaoMap.tsx
+++ src/map/KakaoMap.tsx
@@ -5,7 +5,7 @@ const KakaoMap = () => {
 
   useEffect(() => {
     //window.kakao가 없는경우 ->아직 script가 로드되지 않으면 실행x
-    if (!window.kakao || !mapRef.current) return;
+    if (!window.kakao || !window.kakao) return;
     window.kakao.maps.load(() => {
       const option = {
         center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
--- src/map/KakaoMapScriptLoader.tsx
+++ src/map/KakaoMapScriptLoader.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 const KAKAO_MAP_SCRIPT_ID = "kakao-map-script";
 
@@ -11,15 +11,26 @@ const KakaoMapScriptLoader = ({
   children,
   apiKey,
 }: KakaoMapScriptLoaderProps) => {
+  const [loaded, setLoaded] = useState(false);
   const KAKAO_MAP_SCRIPT_URL = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&autoload=false&libraries=services,clusterer,drawing`;
+  console.log(KAKAO_MAP_SCRIPT_URL);
   useEffect(() => {
     const script = document.createElement("script");
     script.id = KAKAO_MAP_SCRIPT_ID; //중복방지
     script.src = KAKAO_MAP_SCRIPT_URL;
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        console.log("스크립트 로드 완료!!");
+        console.log("window.kakao:", window.kakao);
+        setLoaded(true);
+      });
+    };
+
     document.head.appendChild(script); //script라서 head영역에 넣기
   }, [apiKey]);
 
-  return <>{children}</>;
+  return <>{loaded && children}</>;
 };
 
 export default KakaoMapScriptLoader;
